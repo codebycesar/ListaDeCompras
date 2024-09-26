5508c1d5104780a01f498e826de45bb45e5e33e2
Cesar Sandoval Dev <cesarsandoval.dev@gmail.com>
2024-09-26 02:34:52 -0300

    chore excercice 1

---
+++ app/src/main/java/com/example/listadecompras/CrearListaDeCompras.kt
@@ -0,0 +1,16 @@
+package com.example.listadecompras
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CrearListaDeCompras : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_crear_lista_de_compras)
+
+    }
+}
---
+++ app/src/main/java/com/example/listadecompras/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.listadecompras
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickEnviarUsuario(view: View) {
+        val editText = findViewById<EditText>(R.id.userEditText).text.toString()
+
+
+    }
+}
